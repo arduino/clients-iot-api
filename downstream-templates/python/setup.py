f0d4c4a56d70ad90f8c71cbe631f5e4031ac041c
Massimiliano Pippi <m.pippi@arduino.cc>
2019-10-15 17:42:06 +0200

    add a custom setup.py script (#41)

---
+++ downstream-templates/python/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages  # noqa: H301
+
+version = {}
+with open("arduino_iot_rest/__init__.py") as fp:
+    exec(fp.read(), version)
+
+with open("README.md") as f:
+    long_description = f.read()
+
+NAME = "arduino-iot-client"
+REQUIRES = ["urllib3 >= 1.15", "six >= 1.10", "certifi", "python-dateutil"]
+
+setup(
+    name=NAME,
+    version=version["__version__"],
+    description="Iot API",
+    author_email="",
+    url="",
+    keywords=["OpenAPI", "OpenAPI-Generator", "Iot API"],
+    install_requires=REQUIRES,
+    packages=find_packages(exclude=["test", "tests"]),
+    include_package_data=True,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
