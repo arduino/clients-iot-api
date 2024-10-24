efca54af99dd30c8066da7df7ad1fe4813e90d60
Marco Colombo <ing.marco.colombo@gmail.com>
2024-10-24 14:26:55 +0200

    [WIRE-575] CI/CD refactoring (#94)
    Refactored PR to remove no appigentools and move to a pure openapi-generator implementation

--- downstream-templates/python/utils.py
+++ downstream-templates/python/utils.py
@@ -3,8 +3,7 @@
 """
     Arduino IoT Cloud API
 
-     Provides a set of endpoints to manage Arduino IoT Cloud **Devices**, **Things**, **Properties** and **Timeseries**. This API can be called just with any HTTP Client, or using one of these clients:  * [Javascript NPM package](https://www.npmjs.com/package/@arduino/arduino-iot-client)  * [Python PYPI Package](https://pypi.org/project/arduino-iot-client/)  * [Golang Module](https://github.com/arduino/iot-client-go)  # noqa: E501
-
+    Utility package
 """
 
 import frozendict
---
+++ openapi/compose/compose.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"slices"
+
+	"github.com/getkin/kin-openapi/openapi3"
+	"gopkg.in/yaml.v3"
+
+	_ "embed"
+)
+
+const (
+	BasePath                = "../"
+	OpenapiComponentsFolder = BasePath + "components/"
+	IotApiOpenApi           = OpenapiComponentsFolder + "iotapi-openapi.yaml"
+	HeaderOpenApi           = OpenapiComponentsFolder + "header.yaml"
+	FooterOpenApi           = OpenapiComponentsFolder + "footer.yaml"
+
+	MergedOpenApi = BasePath + "openapi.yaml"
+)
+
+//go:embed path-blacklist.json
+var pathBlacklist string
+
+type blacklist struct {
+	Blacklist []string `json:"blacklist"`
+}
+
+func main() {
+	// Load blacklist
+	var bl blacklist
+	_ = json.Unmarshal([]byte(pathBlacklist), &bl)
+
+	log.Println("Compose OpenAPI...")
+
+	// Load the main (IOT-API) OpenAPI specification
+	loader := openapi3.NewLoader()
+	masterOpenapi, err := loader.LoadFromFile(IotApiOpenApi)
+	if err != nil {
+		log.Fatalf("Failed to load %s: %v", IotApiOpenApi, err)
+	}
+
+	mergedPaths := []openapi3.NewPathsOption{}
+	for _, path := range masterOpenapi.Paths.InMatchingOrder() {
+		if slices.Contains(bl.Blacklist, path) {
+			continue
+		}
+		pathItem := masterOpenapi.Paths.Find(path)
+		mergedPaths = append(mergedPaths, openapi3.WithPath(fmt.Sprintf("/iot%s", path), pathItem))
+	}
+
+	if masterOpenapi.Components.Schemas == nil {
+		masterOpenapi.Components.Schemas = make(map[string]*openapi3.SchemaRef)
+	}
+	if masterOpenapi.Components.Responses == nil {
+		masterOpenapi.Components.Responses = make(map[string]*openapi3.ResponseRef)
+	}
+	if masterOpenapi.Components.Parameters == nil {
+		masterOpenapi.Components.Parameters = make(map[string]*openapi3.ParameterRef)
+	}
+	if masterOpenapi.Components.RequestBodies == nil {
+		masterOpenapi.Components.RequestBodies = make(map[string]*openapi3.RequestBodyRef)
+	}
+	if masterOpenapi.Components.Headers == nil {
+		masterOpenapi.Components.Headers = make(map[string]*openapi3.HeaderRef)
+	}
+	if masterOpenapi.Components.SecuritySchemes == nil {
+		masterOpenapi.Components.SecuritySchemes = make(map[string]*openapi3.SecuritySchemeRef)
+	}
+
+	// Merge the components
+
+	// Load header and footer
+	headerOpenapi, err := loader.LoadFromFile(HeaderOpenApi)
+	if err != nil {
+		log.Fatalf("Failed to load %s: %v", HeaderOpenApi, err)
+	}
+
+	if headerOpenapi.Servers != nil {
+		masterOpenapi.Servers = append(masterOpenapi.Servers, headerOpenapi.Servers...)
+	}
+
+	footerOpenapi, err := loader.LoadFromFile(FooterOpenApi)
+	if err != nil {
+		log.Fatalf("Failed to load %s: %v", FooterOpenApi, err)
+	}
+
+	if footerOpenapi.Components.SecuritySchemes != nil {
+		for name, securityScheme := range footerOpenapi.Components.SecuritySchemes {
+			masterOpenapi.Components.SecuritySchemes[name] = securityScheme
+		}
+	}
+
+	// Add merged paths to the master document
+	masterOpenapi.Paths = openapi3.NewPaths(mergedPaths...)
+
+	ym, err := masterOpenapi.MarshalYAML()
+	if err != nil {
+		log.Fatalf("Failed to marshal merged document to YAML: %v", err)
+	}
+	yamlData, err := yaml.Marshal(ym)
+	if err != nil {
+		log.Fatalf("Failed to marshal merged document to YAML: %v", err)
+	}
+
+	err = os.WriteFile(MergedOpenApi, yamlData, 0644)
+	if err != nil {
+		log.Fatalf("Failed to write %s: %v", MergedOpenApi, err)
+	}
+
+	log.Println("Merged OpenAPI saved to " + MergedOpenApi)
+
+}
